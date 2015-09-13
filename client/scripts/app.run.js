c405a74f08b14acc3ded3ee60ea2475d2d5e9b2f
Scott Jason <scottleejason@gmail.com>
2015-09-13 03:40:36 -0700

    resets state on back btn in run block

--- client/scripts/app.run.js
+++ client/scripts/app.run.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('YouAreHere')
-  .run(['$rootScope', '$location', 'localStorageService', function($rootScope, $location, localStorageService) {
+  .run(['$rootScope', '$state', '$location', 'localStorageService', function($rootScope, $state, $location, localStorageService) {
     var isiPad = navigator.userAgent.match(/iPad/i) != null;
     var isiPhone = !isiPad && ((navigator.userAgent.match(/iPhone/i) != null) || (navigator.userAgent.match(/iPod/i) != null));
     var isiOS = isiPad || isiPhone;
@@ -11,8 +11,10 @@ angular.module('YouAreHere')
       function(event, toState, toParams, fromState, fromParams) {
         var isBackBtn = (fromState.name === 'results' && toState.name === 'search' && !$rootScope.isSearchBtn);
         if (isBackBtn) {
-          $rootScope.isSearchBtn = null;
           event.preventDefault();
+          $rootScope.isSearchBtn = null;
+          $state.transitionTo('results', {reload: false});
+
         }
       }
     )
